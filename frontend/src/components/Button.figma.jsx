a2d2acf0fc120e21509101c6dcbe737998a12ef0
MarcusC <marcusgchan@gmail.com>
2026-03-01 13:38:16 -0800

    code connect

---
+++ frontend/src/components/Button.figma.jsx
@@ -0,0 +1,8 @@
+import figma from "@figma/code-connect"
+
+figma.connect(
+  "https://www.figma.com/design/yTxiuf2brrj3pPSNfo76L0/APITEST?node-id=11-4&t=Kjmcmlrqcwn9bZEw-4",
+  {
+    example: () => <button>Click me</button>,
+  },
+)
---
+++ frontend/src/figmaService.js
@@ -0,0 +1,4 @@
+const FIGMA_TOKEN = "58v52wA5KzV8JKf2N1qfVW"
+const FILE_KEY = "yTxiuf2brrj3pPSNfo76L0"
+npx figma connect create "https://www.figma.com/design/yTxiuf2brrj3pPSNfo76L0/...?node-id=..."
+
