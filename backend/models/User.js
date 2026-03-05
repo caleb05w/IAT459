3d7257d11567c157e26a2fa22cf1b348ee6b3c16
MarcusC <marcusgchan@gmail.com>
2026-03-04 23:55:36 -0800

    added token field to users

--- backend/models/User.js
+++ backend/models/User.js
@@ -10,6 +10,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  figmaToken: {
+    type: String,
+    required: false,
+  },
   fName: {
     type: String,
     required: false,
